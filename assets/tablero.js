10c42df2c50327444386519cc116193c5156fde7
Andres <dibicono@gmail.com>
2024-02-22 11:01:07 -0500

    entregado a dan

--- assets/tablero.js
+++ assets/tablero.js
@@ -3,24 +3,27 @@ let Tablero = document.getElementById("Tablero");
 
 let loading = false;
 
-fetch('https://newsapi.org/v2/everything?q=apple&from=2024-02-21&to=2024-02-21&sortBy=popularity&apiKey=d57a251ca7e64105b0438ba839951d72', { method: "GET" })
+fetch('https://fakestoreapi.com/products/', { method: "GET" })
   .then(response => response.json())
   .then(data => {
     renderizado(data);
-  }).catch(error => console.log(error))
+    console.log(data);
+  })
+  .catch(error => console.log(error))
 
 
 function renderizado(data) {
   console.log("entro a funcion");
   const rawCarts = data;
-  const Cartas = rawCarts.articles;
+  const Cartas = rawCarts;
   console.log(Cartas);
   Cartas.forEach(Carta => {
     Tablero.innerHTML += `
        <div class="shadow p-3 m-2 bg-body-tertiary rounded gap-3" style="width: 25rem;">
-       <img src="${Carta.urlToImage}" class="card-img-top" alt="...">
+       <img src="${Carta.image}" class="card-img-top" alt="...">
        <div class="card-body">
-         <h5 class="card-title">${Carta.title}</h5>
+         <h5 class="card-title">${Carta.price}</h5>
+         <h5 class="card-title">prueba</h5>
          <p class="card-text">${Carta.description}</p>
          <a href="${Carta.url}" class="btn btn-primary">${Carta.source.name}</a>
        </div>
